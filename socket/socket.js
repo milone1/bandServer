43b00ec85eed635871aa2733f5561b43df69a864
Erick Milan <erickmilan1002@gmail.com>
2022-11-06 11:56:26 -0500

    delete band

--- socket/socket.js
+++ socket/socket.js
@@ -19,5 +19,17 @@ io.on('connection',client => {
     client.on('emitir-mensaje', (payload) => {
         io.broadcast.emit('nuevo-mensaje', payload);
     });
-
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands);
+    });
+    client.on('add-band', (payload) => {    
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
+    client.on('delete-band', (payload) => {    
+        bands.deleteBand(payload.id)
+        io.emit('active-bands', bands.getBands());  
+    });
 });
