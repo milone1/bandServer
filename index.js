cbc445cfcff9568c69cb8df82e8530b76d662673
Erick Milan <erickmilan1002@gmail.com>
2022-11-06 15:03:21 -0500

    new badns

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 //* node server
 const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
-require('./socket/socket');
+require('./sockets/socket');
 //* path public 
 //* apunta a donde sea que este montado el servidor.
 const publicPath = path.resolve( __dirname, 'public' );
--- models/band.js
+++ models/band.js
@@ -1,9 +1,15 @@
-const { v4: uuidV4 } = require('uuid')
+const { v4: uuidV4 } = require('uuid');
+
 class Band {
-    constructor(name='no-name') {
-        this.id = uuidV4(); 
-        this.name = name;
+
+    constructor( name = 'no-name' ) {
+        
+        this.id = uuidV4(); // identificador único
+        this.name  = name;
         this.votes = 0;
     }
+
 }
+
+
 module.exports = Band;
