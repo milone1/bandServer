aeca86b401bb924d4144b574c008c6d4447144f2
Erick Milan <erickmilan1002@gmail.com>
2022-11-06 11:16:27 -0500

    add active bands

--- socket/socket.js
+++ socket/socket.js
@@ -1,7 +1,14 @@
 const { io } = require('../index.js');
+const Band = require('../models/band.js');
+const Bands = require('../models/bands.js');
+const bands = new Bands();
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Amarantos'));
+bands.addBand(new Band('Justicy'));
+bands.addBand(new Band('Metall'));
 //* Message sockets
 io.on('connection',client => {
-    console.log('Cliente conectado')
+    client.emit('active-bands', bands.getBands());
     client.on('disconnect', () => {
         console.log('Cliente desconectado.')
     });
